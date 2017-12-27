0d635ec3bd4e90e4eb3316953e70df3fdac5eff2
Dhruv Bhavsar <dhruvhbhavsar@gmail.com>
2017-12-26 21:50:09 -0500

    Initial Commit with CLI Standalone Working

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DotNetCore2StandAlone
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // services.AddMvcCore();
+            // Add framework services.
+            //services.AddMvcCore().AddJsonOptions(options =>
+            //{
+            //    //return json format with Camel Case
+            //    options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+            //});
+            services.AddMvcCore().AddJsonFormatters();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
